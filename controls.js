72fd1d687387f098c3f739ee179b3580e2389393
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2017-05-14 16:03:50 +0200

    Removed some spaghetti

--- controls.js
+++ controls.js
@@ -8,13 +8,9 @@ document.onkeydown = function (e) {
 	if(keys[e.keyCode] == 'Q'){
 		init();
 	} else if(keys[e.keyCode] == 'W'){
-		for(var i = 0; i < 1000; i++){
-			clearTimeout(main_timer);
-		}
+		clearTimeout(main_timer);
 	} else if(keys[e.keyCode] == 'E'){
-		for(var i = 0; i < 1000; i++){
-			clearTimeout(main_timer);
-		}
+		clearTimeout(main_timer);
 		main_timer = setTimeout(main, 0);
 	}
 };
